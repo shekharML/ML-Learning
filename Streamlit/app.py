53f20c853cddb18dbea7cc03571738646849d208
Shekhar Kumar <shekhar.kumar.ext@wsa.com>
2025-02-19 12:35:54 +0530

    Initial Commit

---
+++ Streamlit/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title("Hello World")
+
+st.write("Hello Shekhar")
+
+df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+
+st.write(df)
+
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
